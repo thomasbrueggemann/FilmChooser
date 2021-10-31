5df775a71631bf66bd76f63b819524e4788e1ed6
Thomas Brüggemann <mail@thomasbrueggemann.com>
2021-10-31 07:27:07 +0100

    crawler + website

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:5.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
+WORKDIR /src
+COPY ["FilmChooser.csproj", "./"]
+RUN dotnet restore "FilmChooser.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "FilmChooser.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "FilmChooser.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "FilmChooser.dll"]
---
+++ FilmChooser.FilmCrawler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace FilmChooser.FilmCrawler
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var uniqueFilms = new HashSet<string>();
+            const string url = "https://en.wikipedia.org/wiki/List_of_photographic_films";
+
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            var tables = doc.DocumentNode.SelectNodes("//table[@class='wikitable']");
+
+            foreach (var table in tables)
+            {
+                var rows = table.SelectNodes("/tr");
+            }
+        }
+    }
+}
